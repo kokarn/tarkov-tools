744c6a698724406146f5ed87c7bdf486935eabea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 13:25:31 +0000

    Deploy kokarn/tarkov-tools to github.com/kokarn/tarkov-tools.git:gh-pages

---
+++ precache-manifest.51c2db8dabbad5ac68e9a17f501871ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81d46c3f518eb63e0f0d8bc04344a82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33cf757e8a7aee69bfcf",
+    "url": "./static/css/main.13dfd1bb.chunk.css"
+  },
+  {
+    "revision": "3460ae59943ac22bc545",
+    "url": "./static/js/2.cc3a9e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cc3a9e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33cf757e8a7aee69bfcf",
+    "url": "./static/js/main.71195f29.chunk.js"
+  },
+  {
+    "revision": "e63752ad65770b3ee4f8",
+    "url": "./static/js/runtime-main.24fadf96.js"
+  },
+  {
+    "revision": "4bbaae3d0731a2fc399679d70bbd98a7",
+    "url": "./static/media/bender.4bbaae3d.woff2"
+  },
+  {
+    "revision": "544fd3ad2aa77755edead4647df38060",
+    "url": "./static/media/bender.544fd3ad.woff"
+  }
+]);
